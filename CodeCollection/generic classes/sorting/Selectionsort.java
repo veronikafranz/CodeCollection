f3d5f4aed73ee1173ee8817ae133015c420c6596
veron <veron@LAPTOP-01B6OD61>
2018-09-04 10:48:55 +0200

    added sortMode sortings LinkedList and array

--- CodeCollection/generic classes/sorting/Selectionsort.java
+++ CodeCollection/generic classes/sorting/Selectionsort.java
@@ -3,7 +3,66 @@ package sorting;
 import java.util.LinkedList;
 
 public class Selectionsort<T extends Comparable<T>> {
+	
+	final static String ASC = "ASC";
+	final static String DESC = "DESC";
+	
+	public void sort(T[] array) {
+		//case: array is empty
+		if(array == null || array.length <= 0) {
+			return;
+		}
+		//case: array contains only 1 element
+		if(array.length == 1) {
+			return;
+		}
+		//sort default mode ASC
+		sort("ASC", array);		
+	}
+	
+	public void sort(String sortMode, T[] array) {
+		//case: array is empty
+		if(array == null || array.length <= 0) {
+			return;
+		}
+		//case: array contains only 1 element
+		if(array.length == 1) {
+			return;
+		}
+		//sort
+		for(int i = 0; i < array.length - 1; i++) {
+			//position start entry for comparison
+			int indexEntry = 0;
+			if(sortMode == "ASC" || sortMode == null) {
+				for(int j = 1; j < array.length - i; j++) {
+					//if entry j is bigger than entryToCompare
+					if(array[j].compareTo(array[indexEntry]) > 0) {
+						//new position of entry to compare
+						indexEntry = j;
+					}
+				}
+			} else if(sortMode == "DESC") {
+				for(int j = 1; j < array.length - i; j++) {
+					//if entry j is smaller than entryToCompare
+					if(array[j].compareTo(array[indexEntry]) < 0) {
+						//new position of entry to compare
+						indexEntry = j;
+					}
+				}
+			} else {
+				throw new IllegalArgumentException("Please choose correct sort mode.");
+			}
+			//if indexMax is not last position of currently unsorted entries
+			if(indexEntry != array.length - 1 - i) {
+				//switch
+				T temp = array[array.length - 1 - i];
+				array[array.length - 1 - i] = array[indexEntry];
+				array[indexEntry] = temp;				
+			}
+		}
+	}
 
+	
 	//TODO test
 	public void sortAscending(T[] array) {
 
@@ -50,6 +109,65 @@ public class Selectionsort<T extends Comparable<T>> {
 		}
 	}
 	
+	//TODO test
+	public void sort(LinkedList<T> list) {
+		//case: list empty
+		if(list.size() == 0 || list == null) {
+			return;
+		}
+		//case: list contains 1 element
+		if(list.size() == 1) {
+			return;
+		}
+		//sort default mode ASC
+		sort("ASC", list);
+	}
+	
+	//TODO test
+	public void sort(String sortMode, LinkedList<T> list) {
+		//case: list empty
+		if(list.size() == 0 || list == null) {
+			return;
+		}
+		//case: list contains 1 element
+		if(list.size() == 1) {
+			return;
+		}
+		//sort
+		for(int i = 0; i < list.size() - 1; i++) {
+			//position entryToCompare
+			int indexEntry = 0;
+			
+			for(int j = 1; j < list.size() - i; j++) {
+				if(sortMode == ASC || sortMode == null) {
+					//compare current with entryToCompare entry
+					//if entry entryToCompare is smaller than j-entry
+					if(list.get(indexEntry).compareTo(list.get(j)) < 0) {
+						//switch entryToCompare
+						indexEntry = j;
+					}
+				} else if(sortMode == DESC) {
+					//compare current with entryToCompare entry
+					//if entry entryToCompare is bigger than j-entry
+					if(list.get(indexEntry).compareTo(list.get(j)) > 0) {
+						//switch entryToCompare
+						indexEntry = j;
+					}
+				} else {
+					throw new IllegalArgumentException("Please choose correct sort mode.");
+				}
+			}
+			//if entryToCompare is not last position of currently unsorted entries
+			if(indexEntry != list.size() - 1 - i) {
+				//switch entries
+				T temp = list.get(list.size() - 1 - i);
+				list.set(list.size() - 1 - i, list.get(indexEntry));
+				list.set(indexEntry, temp);
+			}
+		}
+	}
+
+
 	//TODO test
 	public void sortAscending(LinkedList<T> list) {
 		
